0a548ea82d3b7962bc15d580cecfa2cbd2d82163
fabsouto <fabsouto@cl.ibm.com>
2017-11-17 11:41:35 -0300

    Change "comment" path to "comments"

--- electivos/urls.py
+++ electivos/urls.py
@@ -7,7 +7,7 @@ app_name = 'electivos'
 
 urlpatterns = [
     url(r'^courses$', views.CoursesView.as_view(), name="courses"),
-    url(r'^courses/comment$', csrf_exempt(views.CommentView.as_view()), name="comment"),
-    url(r'^courses/comment/like$', csrf_exempt(views.LikeView.as_view()), name="like"),
-    url(r'^courses/comment/dislike$', csrf_exempt(views.DislikeView.as_view()), name="dislike")
+    url(r'^courses/comments$', csrf_exempt(views.CommentView.as_view()), name="comment"),
+    url(r'^courses/comments/like$', csrf_exempt(views.LikeView.as_view()), name="like"),
+    url(r'^courses/comments/dislike$', csrf_exempt(views.DislikeView.as_view()), name="dislike")
 ]
